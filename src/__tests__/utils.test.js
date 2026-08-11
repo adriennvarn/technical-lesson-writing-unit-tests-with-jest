7607aa4e19726a7dead73ac72b0ff416b4a6f211
Adrienn Varn <adriennvarn@gmail.com>
2026-08-11 18:04:01 -0400

    completed

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,19 @@
-// Your tests here
+import { pointsForWord } from "../utils"
+
+describe("pointsForWord", () => {
+    it("calculates the total points per word (1 for vowel, 2 for consonant)", () => {
+        // Define test val
+        const word = "test"
+
+        // Pass test value as argument
+        const points = pointsForWord(word)
+
+        // Check that resturn value is 7
+        expect(points).toBe(7)
+    },
+    it("ensure calculation is case insensitive", () => {
+        const word = "TeSt"
+        const points = pointsForWord(word)
+        expect(points).toBe(7)
+    }))
+})
